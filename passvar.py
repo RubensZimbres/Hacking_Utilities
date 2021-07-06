814617d2595c19ad6ad0ba2e81de934fe7e7d342
Rubens Zimbres <rubenszmm@gmail.com>
2021-07-06 14:16:38 -0300

    Update passvar.py

--- passvar.py
+++ passvar.py
@@ -8,12 +8,15 @@ parser.add_argument('--f', dest='file', type=str, default=0, help='Path of input
 parser.add_argument('--d', dest='output-dir', type=str, default=0, help='Path of output directory')
 args = vars(parser.parse_args())
 
+def colored(r, g, b, text):
+    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+print('\n')
 x=0
 outcome=[]
 with open(args['file'],'r', encoding="latin-1") as f:
     my_list = list(f)
     my_list = [x.rstrip() for x in my_list] 
-    print("[+] Length of input password dictionary",len(my_list),'\n')
+    print(colored(255,165,0,"[+] Length of input password dictionary:"),colored(255,165,0,len(my_list)),'\n')
     for word in my_list:
         try:
             x+=1
@@ -86,9 +89,9 @@ with open(args['file'],'r', encoding="latin-1") as f:
             
         except:
             pass
-print("[+] Length of output password dictionary",len(outcome),'\n')
+print(colored(255,165,0,"[+] Length of enriched password dictionary:"),colored(255,165,0,len(outcome)),'\n')
 
-print("[+] Saving output . . .",'\n')
+print(colored(255,165,0,"[+] Saving output . . ."),'\n')
 
 n=14000000  ## Number of lines of each txt file generated
 for i in range(0, len(outcome), n):
@@ -96,4 +99,4 @@ for i in range(0, len(outcome), n):
             f.write("\n".join(map(str, outcome[i:i + n]
         )))
 
-print("[+] Variance generation COMPLETED !",'\n')
+print(colored(0,255,0,"[+] Variance generation COMPLETED !"),'\n')
